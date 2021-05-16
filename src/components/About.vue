35b982de9f27abb6c6288b6ae8a1f6b4ac393fdd
patchanop13 <59010933@kmitl.ac.th>
2021-05-16 22:08:00 +0700

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Version 1.0.0</h1>
+      <router-link to="/">GO Back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
